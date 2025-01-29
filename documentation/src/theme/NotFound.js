1477ceddb1574f606666242a5c20cf51b6341a22
Adewale Abati <acekyd01@gmail.com>
2025-01-29 02:13:29 +0100

    final try

--- documentation/src/theme/NotFound.js
+++ documentation/src/theme/NotFound.js
@@ -5,7 +5,7 @@ export default function NotFound() {
   const location = useLocation();
 
   // Ignore 404 handling when inside `/v1/extensions/`
-  if (location.pathname.startsWith('/goose/v1/extensions')) {
+  if (location.pathname.includes('v1/extensions')) {
     return null; // Don't show anything, let GitHub Pages serve the React app
   }
 
